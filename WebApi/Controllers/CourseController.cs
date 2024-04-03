298ebf1b7a11aec3888b326ed8b949b07d1819de
Annelieno1 <gydano1@hotmail.com>
2024-04-03 08:48:21 +0200

    first commit

---
+++ WebApi/Controllers/CourseController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+    }
+}
